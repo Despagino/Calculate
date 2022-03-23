586e5b862166287dbae602322f456ea3926127a1
Gino Tasis <gtasis84@gmail.com>
2022-03-23 11:48:30 -0400

    Create panels for the user input and the result

--- Calculate/ContentView.swift
+++ Calculate/ContentView.swift
@@ -10,14 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .padding()
+            HStack {
+                Spacer()
+                Text("Hello, world!")
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 30, weight: .heavy))
+            }
+            HStack {
+                Spacer()
+                Text("Hello, world!")
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 40, weight: .heavy))
+            }
         }
-    }
+        .background(Color.black.ignoresSafeArea())
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        }
     }
 }
